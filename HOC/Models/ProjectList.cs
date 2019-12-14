3166df09984bf4b1c4ca3396b5a1f40e3da1daea
David Yi <david.yi@lcginc.com>
2019-12-13 19:29:35 -0500

    add 2 new clasees projectList na d researchproject

---
+++ HOC/Models/ProjectList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOC.Models
+{
+    public class cProjectList
+    {
+        List<sProjectInfo> pList;
+        public cProjectList() {
+            pList = new List<sProjectInfo>();
+        }
+
+        public int ProjectCount
+        {
+            get
+            {
+                return pList.Count;
+            }
+        }
+
+        public bool GetProjects(int organizerID)
+        {
+            //if (DBObj.GetProjects(organizerID, out List<sProjectInfo> plist))
+            {
+                //FillProjectListData
+
+                return true;
+            }
+
+            return false;
+        }
+
+        void FillProjectListData()
+        {
+
+
+        }
+
+    }
+}
