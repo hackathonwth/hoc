333a151a3d116b4d6e9a11ec6671cbea11c57046
hackathonadmin <hackathonadmin@WTH1>
2019-12-14 19:11:29 -0500

    Adding workflow to organizer controller

---
+++ HOC.Entities/Models/DB/Workflow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HOC.Entities.Models.DB
+{
+    public class Workflow
+    {
+        public Workflow()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
